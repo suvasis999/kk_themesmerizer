99dca0f3923cbca4988dda5b199b0b5f5201dacc
suvasis999 <talk2suvasis@gmail.com>
2022-06-13 12:24:07 +0530

    'updates'

--- service/BaseURL.tsx
+++ service/BaseURL.tsx
@@ -2,5 +2,5 @@
 //export const IMAGE_URL = "http://localhost:1337";
 
 
-export const BASE_URL = "https://85.25.211.157:1337/api";
-export const IMAGE_URL = "https://85.25.211.157:1337";
+export const BASE_URL = "http://85.25.211.157:1337/api";
+export const IMAGE_URL = "http://85.25.211.157:1337";
